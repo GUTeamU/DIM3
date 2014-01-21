18df0995886f10f22fcf6b4c7881cfff1380d11d
Edvin Malinovskis <edvin.malinovskis@gmail.com>
2014-01-21 16:55:32 +0000

    Implemented basic models and enabled admin interface

--- dim3/dim3/settings.py
+++ dim3/dim3/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
+    'rct'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,4 +80,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.messages.context_processors.messages',
+    'django.contrib.auth.context_processors.auth',
+)
--- dim3/rct/models.py
+++ dim3/rct/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField()
+    description = models.TextField()
+    project = models.ForeignKey(Project)
+
+    def __unicode__(self):
+        return self.name
