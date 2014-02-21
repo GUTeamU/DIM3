83154e4183ea4494fe1ace62dd1a2a7e591d9bcb
Michael Cromie <michaelcromie22@gmail.com>
2014-02-21 18:35:03 +0000

    Made some progress with projectBoard

---
+++ DIMSite/projectBoard.js
@@ -0,0 +1,19 @@
+function mustHave() {
+	var elem = document.getElementById("whiteboard");
+	elem.style.background = "red";
+}
+
+function shouldHave() {
+	var elem = document.getElementById("whiteboard");
+	elem.style.background = "orange";
+}
+
+function couldHave() {
+	var elem = document.getElementById("whiteboard");
+	elem.style.background = "green";
+}
+
+function wouldLike() {
+	var elem = document.getElementById("whiteboard");
+	elem.style.background = "#4b819a";
+}
