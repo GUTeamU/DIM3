eba264a4f8f85892d45cff66693139927fc211fc
Andrew McDonald <mcdonalda1993@gmail.com>
2014-03-15 21:04:56 +0000

    Fixed signing up. It used to not take in the users password and so all passwords were blank. Thus logging in is now fixed

--- requirementsTracker/rct/forms.py
+++ requirementsTracker/rct/forms.py
@@ -8,7 +8,7 @@ class UserForm(forms.ModelForm):
 	
 	class Meta:
 		model = User
-		fields = ('username','first_name','last_name','email')
+		fields = ('username', 'password', 'first_name','last_name','email')
 
 class ProjectForm(forms.ModelForm):
 
