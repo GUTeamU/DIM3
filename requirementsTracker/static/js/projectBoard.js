2a15a11c8470477ecc1e21dd98e2a565872bb4a4
Michael W Cromie <michaelcromie22@gmail.com>
2014-03-17 19:47:07 +0000

    colour of 'boxes' div still does not update correctly

--- requirementsTracker/static/js/projectBoard.js
+++ requirementsTracker/static/js/projectBoard.js
@@ -1,35 +1,45 @@
 function mustHave() {
-	var elem = document.getElementById("whiteboard");
-	elem.style.borderColor = "#FF0000";
+	document.getElementById("whiteboard").style.borderColor = "#FF0000";
+	document.getElementById("boxes").style.backgroundColor = "#FF0000";
+
 	$("#should").hide()
 	$("#could").hide()
 	$("#would").hide()
 	$("#must").show()
-}
+
+};
 
 function shouldHave() {
-	var elem = document.getElementById("whiteboard");
-	elem.style.borderColor = "#FF8330";
+	document.getElementById("whiteboard").style.borderColor = "#FF8330";
+	document.getElementById("boxes").style.backgroundColor = "#FF8330";
+	
 	$("#must").hide()
 	$("#could").hide()
 	$("#would").hide()
 	$("#should").show()
-}
+
+
+};
 
 function couldHave() {
-	var elem = document.getElementById("whiteboard");
-	elem.style.borderColor = "#19A319";
+	document.getElementById("whiteboard").style.borderColor = "#19A319";
+	document.getElementById("boxes").style.backgroundColor = "#19A319";
+
 	$("#must").hide()
 	$("#should").hide()
 	$("#would").hide()
 	$("#could").show()
-}
+
+};
 
 function wouldLike() {
-	var elem = document.getElementById("whiteboard");
-	elem.style.borderColor = "#1693BC";
+	document.getElementById("whiteboard").style.borderColor = "#1693BC";
+	document.getElementById("boxes").style.backgroundColor = "#1693BC";
+
 	$("#must").hide()
 	$("#should").hide()
 	$("#could").hide()
 	$("#would").show()
-}
+
+};
+
